d67c4924d5416579e3af9ea6f0f424c01784ac8d
Kennedy Ramos <devnedy@gmail.com>
2025-09-17 20:15:43 -0300

    ajustando código e arquivos

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './app'
-import './index.css' // <- importante
-
-createRoot(document.getElementById('root')).render(<App />)
