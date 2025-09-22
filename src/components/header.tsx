cc9f894227763fe8cab37e6404d1086065cafd39
Kennedy Ramos <devnedy@gmail.com>
2025-09-22 20:58:22 -0300

    Criando os componentes header e card-imovel, mudança das cores do dashboarde de indigo para blue, acrescentando informação de dia de pagamento nos cards

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="text-center p-4 font-bold">
+      <h1 className="text-2xl  text-blue-800">Meu Imóvel</h1>
+    </header>
+  );
+}
