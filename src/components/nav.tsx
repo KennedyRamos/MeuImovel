819aa88e46f51c3f2f77076ee109358b95408d9c
Kennedy Ramos <devnedy@gmail.com>
2025-09-25 19:43:08 -0300

    Criando a rota para a page inquilinos, colocando o linque no menu

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,10 +2,18 @@ export default function Nav() {
   return (
     <nav className="bg-blue-400 bottom-0 left-0 w-screen fixed">
         <ul className="w-full grid grid-cols-4 text-center py-5 text-white">
-            <li><a href="/Dashboard">Dashboard</a></li>
-            <li><a href="/Imoveis">Imóveis</a></li>
-            <li>Inquilinos</li>
-            <li>Contratos</li>
+            <li>
+              <a href="/Dashboard">Dashboard</a>
+            </li>
+            <li>
+              <a href="/Imoveis">Imóveis</a>
+            </li>
+            <li>
+              <a href="/Inquilinos">Inquilinos</a>
+            </li>
+            <li>
+              <a href="/Contratos">Contratos</a>
+            </li>
         </ul>
     </nav>
   )
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,13 +2,19 @@ import { Routes, Route } from "react-router-dom"
 import Login from "../pages/Login"
 import Dashboard from "../pages/Dashboard"
 import Imoveis from "../pages/Imoveis"
+import Inquilinos from "../pages/Inquilinos"
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+
       <Route path="/dashboard" element={<Dashboard />} />
+
       <Route path="/Imoveis" element={<Imoveis/>} />
+
+      <Route path="/Inquilinos" element={<Inquilinos/>} />
+
     </Routes>
   )
 }
