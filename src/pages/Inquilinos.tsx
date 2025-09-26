ffa8f86747421986895ae26b7a1b003678771c12
Kennedy Ramos <devnedy@gmail.com>
2025-09-26 20:51:10 -0300

    criando o card inquilino

--- src/pages/Inquilinos.tsx
+++ src/pages/Inquilinos.tsx
@@ -1,4 +1,6 @@
 import Header from "../components/header";
+import Pesquisa from "../components/barra-pesquisa";
+import CardInquilinos from "../components/card-inquilinos";
 import Nav from "../components/nav";
 
 export default function Inquilinos() {
@@ -7,7 +9,11 @@ export default function Inquilinos() {
             <Header></Header>
 
             <main  className="mx-4 grid gap-10">
-                
+                <h2 className="text-center text-3xl font-semibold">Inquilinos</h2>
+                <section>
+                    <Pesquisa></Pesquisa>
+                    <CardInquilinos></CardInquilinos>
+                </section>
             </main>
 
            <Nav></Nav> 
