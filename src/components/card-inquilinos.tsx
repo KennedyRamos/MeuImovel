c845bd22e35cec19441438f4fc6058397e7596b3
Kennedy Ramos <devnedy@gmail.com>
2025-09-28 14:27:15 -0300

    terminando a estilização do card inquilinos

--- src/components/card-inquilinos.tsx
+++ src/components/card-inquilinos.tsx
@@ -5,19 +5,19 @@ import { faAngleUp } from "@fortawesome/free-solid-svg-icons"
 export default function cardInquilinos () {
     return(
         <div className="grid gap-10">
-            <div className="shadow-xl rounded-lg grid grid-cols-2 gap-3 text-center p-2 border-5 border-blue-100">
+            <div className="shadow-xl rounded-lg grid grid-cols-2 gap-3 text-center p-2 border-y-5 border-blue-400">
                 <div>
-                    <p className="text-xl font-semibold">Ayrton Ferraz</p>
+                    <p className="text-2xl font-semibold">Ayrton Ferraz</p>
                     <h3 className="font-medium text-blue-800">Inquilino</h3>
                 </div>
 
                 <div>
-                    <p className="text-xl font-semibold">5</p>
+                    <p className="text-2xl font-semibold">5</p>
                     <h3 className="font-medium text-blue-800">Alugueis</h3>
                 </div>
 
                 <button className="col-span-2">
-                    <FontAwesomeIcon icon={faAngleDown} className="text-blue-400 text-2xl" />
+                    <FontAwesomeIcon icon={faAngleDown} className="text-white text-2xl p-2 bg-blue-400 rounded-xl" />
                 </button>
             </div>
         </div>
