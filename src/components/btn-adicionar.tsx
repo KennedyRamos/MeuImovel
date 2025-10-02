71315214ef3c29c63a080829fae922803cf869d2
Kennedy Ramos <devnedy@gmail.com>
2025-10-02 17:19:12 -0300

    colocando o botão adicionar nas pages

--- src/components/btn-adicionar.tsx
+++ src/components/btn-adicionar.tsx
@@ -2,6 +2,6 @@ import { Icons } from "../icons/icons"
 
 export default function Adicionar(){
     return(
-        <Icons.Adicionar/>
+        <Icons.Adicionar className="bg-blue-400 p-1 rounded-sm text-white text-2xl"/>
     )
 }
--- src/pages/Imoveis.tsx
+++ src/pages/Imoveis.tsx
@@ -2,16 +2,20 @@ import Header from "../components/header";
 import Pesquisa from "../components/barra-pesquisa";
 import CardImovel from "../components/card-imovel";
 import Nav from "../components/nav";
+import Adicionar from "../components/btn-adicionar";
 
 export default function Imoveis() {
     return(
         <div>
             <Header></Header>
 
-            <main className="mx-4 bg-gray-100">
-                <h2 className="text-center text-3xl font-semibold">Imóveis</h2>
+            <main className="mx-4 bg-gray-100">               
                 <section className="mb-20">
                     <Pesquisa></Pesquisa>
+                    <div className="flex justify-between my-5">
+                        <h2 className="text-center text-2xl font-semibold">Imóveis</h2>
+                        <Adicionar></Adicionar>
+                    </div>
                     <CardImovel></CardImovel>
                 </section>
                 
