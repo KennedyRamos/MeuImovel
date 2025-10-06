2e5ad1c1449371d85829f007a54678329b1fcfbb
Kennedy Ramos <devnedy@gmail.com>
2025-10-06 19:01:21 -0300

    ajuste no espaçamento dos cards, tamnho ocupado pela main mesmo quando não tem conteúdo, e finalizando responsividade da page inquilinos

--- src/pages/Inquilinos.tsx
+++ src/pages/Inquilinos.tsx
@@ -6,12 +6,12 @@ import Adicionar from "../components/btn-adicionar";
 
 export default function Inquilinos() {
     return(
-        <div>
+        <div className="min-h-screen bg-gray-100 flex flex-col">
             <Header></Header>
 
-            <main  className="mx-4 bg-gray-100">
+            <main  className="w-full flex-1 mx-auto px-4 grid gap-10 lg:w-2/3 shadow-xl/50">
                 
-                <section>
+                <section className="pb-20">
                     <Pesquisa></Pesquisa>
                     <div className="flex justify-between my-5">
                         <h2 className="text-center text-2xl font-semibold">Inquilinos</h2>
